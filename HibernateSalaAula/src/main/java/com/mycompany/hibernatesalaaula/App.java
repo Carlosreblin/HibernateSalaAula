f9a29e9ec3e30d848149267b219f3fcbbf79eca5
aluno <aluno@lab01-22>
2026-09-21 09:03:38 -0300

    Update main class name

---
+++ HibernateSalaAula/src/main/java/com/mycompany/hibernatesalaaula/App.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hibernatesalaaula;
+
+/**
+ *
+ * @author aluno
+ */
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
